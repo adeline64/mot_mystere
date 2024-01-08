ab7047adcd6147a287af8062073aa14f5a480c11
adeline <adeline.lacabanne@gmail.com>
2024-01-08 21:31:43 +0100

    first commit

--- code/php/mystery_word.php
+++ code/php/mystery_word.php
@@ -1,3 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-// Vous devez faire un select pour afficher le mot mystere affiché en bdd ! =)
+    <?php
+
+    // Vous devez faire un select pour afficher le mot mystere affiché en bdd ! =)
+
+    ?>
+
+    <p> Je veux connaitre le mot mystere, donc affichez le ici : <?php //juste la ?></p>
+    
+</body>
+</html>
